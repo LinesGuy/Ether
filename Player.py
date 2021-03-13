b62d39467aeaccbc23f8c9f0bfd2b2c4dc50dc6d
LinesGuy <60029482+LinesGuy@users.noreply.github.com>
2021-03-13 22:28:43 +0000

    Add Burst combat art

--- Player.py
+++ Player.py
@@ -5,7 +5,9 @@ from entity_abc import Entity
 from user_input import Input
 from bullet import Bullet
 from combat_arts.burn import ArtBurn
+from combat_arts.burst import ArtBurst
 import entity_manager
+from camera import Camera
 from art import player_img
 
 class Player(Entity):
@@ -14,13 +16,25 @@ class Player(Entity):
     def __init__(self, pos=pg.Vector2(0, 0)):
         super().__init__(pos, player_img)
         self.radius = 10
-        self.cooldown_frames = 6
+        self.cooldown_frames = 5
         self.cooldown_remaining = 0
+        self.frames_until_respawn = 0
+
+    def is_dead(self):
+        """Returns whether or not the player is dead based off their
+        frames_until_respawn attributes"""
+        return self.frames_until_respawn > 0
+
+    def kill(self):
+        """Kills the player"""
         self.frames_until_respawn = 0
 
     def update(self):
         """Update player position"""
-        # check ded here
+
+        if self.is_dead():
+            self.frames_until_respawn -= 1
+            return
 
         # Movement stuffs
         speed = 3
@@ -30,11 +44,33 @@ class Player(Entity):
         # Here I would clamp the player pos but I plan to instead
         # move the camera around.
 
+        # Set orientation
         if velocity != pg.Vector2(0, 0):
             self.orientation = velocity.angle_to(pg.Vector2(0, 0))
 
+        # Pew pew
+        if Input.get_mouse_state()[0]:
+            # If holding left click...
+            aim_unit = Input.get_aim_direction(self.pos*1)
+            if (aim_unit != pg.Vector2(0, 0)
+                and self.cooldown_remaining <= 0):
+                self.cooldown_remaining = self.cooldown_frames
+                velocity = aim_unit * 5  # bullet_speed = 5
+                offset_pos = self.pos + aim_unit * 5 + aim_unit.rotate(90) * 2.5
+                entity_manager.EntityManager.add(Bullet(offset_pos, velocity))
+                offset_pos = self.pos + aim_unit * 5 - aim_unit.rotate(90) * 2.5
+                entity_manager.EntityManager.add(Bullet(offset_pos, velocity))
+
+        if self.cooldown_remaining > 0:
+            self.cooldown_remaining -= 1
+
+        # '1' to 'burn':
         if Input.check_just_pressed(pg.K_1):
-            bullet_speed = 2
             entity_manager.EntityManager.add(ArtBurn(self.pos*1,
                 Input.get_aim_bearing(self.pos)))
-            print(1)
+
+        # '2' to 'burst'
+        if Input.check_just_pressed(pg.K_2):
+            entity_manager.EntityManager.add(ArtBurst(self.pos*1,
+                Input.get_aim_bearing(self.pos),
+                destination=Camera.get_mouse_coords()))
---
+++ combat_arts/burst.py
@@ -0,0 +1,36 @@
+import pygame as pg
+from bullet import Bullet
+from user_input import Input
+import entity_manager
+import random
+from camera import Camera
+
+class ArtBurst(Bullet):
+    """The 'burst' combat art, shoots a large bullet that explodes into
+    multiple smaller bullets"""
+
+    def __init__(self, pos, direction: float, destination):
+        # direction is in degrees
+        # destination is coordinates for where to 'explode'
+        BULLET_SPEED = 5
+        velocity = pg.Vector2(1,0).rotate(direction) * BULLET_SPEED
+        #pos = pos + velocity * 5  # Spawn bullet slightly in front of player
+        lifespan = round(pos.distance_to(destination) / BULLET_SPEED)
+        super().__init__(pos, velocity, lifespan=lifespan)
+
+    def update(self):
+        self.pos += self.velocity
+
+        self.age += 1
+        if self.age > self.lifespan:
+            for i in range(10):
+                bearing = random.random() * 360
+                velocity = pg.Vector2(1, 0).rotate(bearing) * 5
+                entity_manager.EntityManager.add(Bullet(self.pos*1, velocity))
+            self.is_expired = True
+
+    def draw(self, screen):
+        screen_pos = self.pos - Camera.pos
+
+        # Draw debug circle
+        pg.draw.circle(screen, (0, 0, 255), screen_pos, 3)
